953214128480a4c1d9f998091b424d4b4965a3e6
Jean-Baptiste PENRATH <jb.penrath@gmail.com>
2017-11-12 14:59:29 +0100

    💄 refactor(Todos): fix some eslint error

--- src/components/views/Todos/Todos.jsx
+++ src/components/views/Todos/Todos.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { List } from 'immutable';
-import TodoList from 'containers/TodoList';
-import AddTodo from 'containers/AddTodo';
-import VisibilityFilter from 'containers/VisibilityFilter';
+import TodoList from 'containers/TodoList.jsx';
+import AddTodo from 'containers/AddTodo.jsx';
+import VisibilityFilter from 'containers/VisibilityFilter.jsx';
 
-const Todos = ({ loading, todos = [] }) => {
-  return (
-    <div className="Todos">
-      <header className="Todos-header">
-        <h1>To Do Tasks</h1>
-      </header>
-      <section className="Todos-container">
-        <AddTodo />
-        {loading ?
-          <span className="loader" /> :
-          <TodoList todos={todos} />
-        }
-      </section>
-      <footer className="Todos-footer">
-        <h3 className="Todos-counter">{todos.size} todos</h3>
-        <VisibilityFilter className="Todos-filter" />
-      </footer>
-    </div>
-  );
-};
+const Todos = ({ loading, todos = [] }) => (
+  <div className="Todos">
+    <header className="Todos-header">
+      <h1>To Do Tasks</h1>
+    </header>
+    <section className="Todos-container">
+      <AddTodo />
+      {loading ?
+        <span className="loader" /> :
+        <TodoList todos={todos} />
+      }
+    </section>
+    <footer className="Todos-footer">
+      <h3 className="Todos-counter">{todos.size} todos</h3>
+      <VisibilityFilter className="Todos-filter" />
+    </footer>
+  </div>
+);
 
 Todos.propTypes = {
   loading: PropTypes.bool.isRequired,
@@ -36,14 +34,6 @@ Todos.defaultProps = {
   todos: List(),
 };
 
-const mapStateToProps = (state) => {
-  const nextState = {
-    loading: state.todoApp.get('loading'),
-    todos: filterTodos(state.visibilityFilter, state.todoApp.get('todos')),
-  };
-  return nextState;
-};
-
 function filterTodos(filter, todos) {
   switch (filter) {
     case 'SHOW_ACTIVE':
@@ -52,7 +42,14 @@ function filterTodos(filter, todos) {
     default:
       return todos;
   }
-
 }
+
+const mapStateToProps = (state) => {
+  const nextState = {
+    loading: state.todoApp.get('loading'),
+    todos: filterTodos(state.visibilityFilter, state.todoApp.get('todos')),
+  };
+  return nextState;
+};
 
 export default connect(mapStateToProps)(Todos);
